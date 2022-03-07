35fef9d222a958a59c9aebf2ac594b69d623eb04
HINDHUJHA S <91025603+Hindhujha@users.noreply.github.com>
2022-03-07 21:59:23 +0530

    Delete Indexes directory

--- Indexes/Indexes.sql
+++
@@ -1,9 +0,0 @@
-use Practice
-
-select * from hired_candidates 
-
-create NonClustered Index IX_hired_candidates_Id
-on hired_candidates (first_name ASC)
-
-select first_name from hired_candidates 
-
